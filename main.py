342f973b1172196530e27546cc2eeafd87d8dfe5
sudo-ashu <rathoreashutosh3006@gmail.com>
2026-04-24 07:06:33 +0530

    refining main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
-import compression_spec_scale_info
-import plottings
+import compression_spec_scale_info as comp_spec
+import plottings as plts
 import shannon_eegfilt_partition_findPair_substitute as bts
 
 file_path = "F056.txt"
@@ -17,8 +17,8 @@ X_filt = bts.eegfilt_equivalent(X, fs, lowcut, highcut)
 X_filt = X_filt[50:-50]
 
 # COMPRESSION SPECTRUM
-print(len(compression_spec_scale_info.compression_spectrum_scale_info_NEW(X_filt, 4)))
-comp_ratio, N, scale, scale_comp_cell, Ent = compression_spec_scale_info.compression_spectrum_scale_info_NEW(X_filt, 4)
+print(len(comp_spec.compression_spectrum_scale_info_NEW(X_filt, 4)))
+comp_ratio, N, scale, scale_comp_cell, Ent = comp_spec.compression_spectrum_scale_info_NEW(X_filt, 4)
 
 print("ETC value:", N)
 print("Spectrum bandwidth:", np.count_nonzero(comp_ratio))
@@ -26,9 +26,9 @@ print("Max scale:", max(scale))
 print("Mean fluctuation:", np.mean(np.abs(np.diff(scale))))
 
 # PLOTS
-plottings.plot_all(comp_ratio, scale, Ent)
-plottings.plot_frequency_spectrum(X_filt, fs)
-plottings.plot_compression_vs_scale(comp_ratio)
-plottings.plot_entropy_vs_scale(Ent)
+plts.plot_all(comp_ratio, scale, Ent)
+plts.plot_frequency_spectrum(X_filt, fs)
+plts.plot_compression_vs_scale(comp_ratio)
+plts.plot_entropy_vs_scale(Ent)
 
 # return comp_ratio, scale, Ent
