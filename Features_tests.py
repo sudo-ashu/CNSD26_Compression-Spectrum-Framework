82155446ee20526b9e69c40664692b18294e8d1b
sudo-ashu <rathoreashutosh3006@gmail.com>
2026-06-23 19:34:50 +0530

    did ANOVA & Kruksal-Wallis testing for the features

--- Features_tests.py
+++ Features_tests.py
@@ -1,5 +1,5 @@
 from scipy import stats
-from scipy.stats import shapiro
+from scipy.stats import shapiro, f_oneway, kruskal
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -31,7 +31,7 @@ for feature in features:
         print(f"{state:15s}  p = {p:.4f}")
 
 
-# visual test: Q-Q plot
+########### visual test: Q-Q plot ##########
 stats.probplot(
     sleep_df["ETC"],
     dist="norm",
@@ -40,3 +40,43 @@ stats.probplot(
 
 plt.title("Sleep - Bandwidth Q-Q Plot")
 plt.show()
+
+
+############################################
+# using z_score test to test for outliers
+
+from scipy.stats import zscore
+
+sleep_df["ETC_z"] = zscore(sleep_df["ETC"])
+sleep_df["Entropy_z"] = zscore(sleep_df["mean_entropy"])
+
+print(sleep_df[np.abs(sleep_df["ETC_z"]) > 3])
+print(sleep_df[np.abs(sleep_df["Entropy_z"]) > 3])
+
+sleep_clean = sleep_df[np.abs(sleep_df["ETC_z"]) < 3]
+stat, p = shapiro(sleep_clean["ETC"])
+print(p)
+
+###########################################
+# ANOVA test for the anova-group
+
+for feature in features[1:4]:
+    F, p = f_oneway(
+        awake_df[feature],
+        sleep_df[feature],
+        anes_df[feature]
+    )
+    print("F-statistic =", F)
+    print("p-value =", p)
+
+###########################################
+# Kruksal-Wallis test for the ETC
+
+H, p = kruskal(
+    awake_df["ETC"],
+    sleep_df["ETC"],
+    anes_df["ETC"]
+)
+
+print("H-statistic =", H)
+print("p-value =", p)
